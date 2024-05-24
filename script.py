26b493fe41e70dfbc4b922b776aeb5096ecfd74a
Muhammad Husnain <bitsol44@gmail.com>
2024-05-24 05:34:00 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import re
 
-string = "https://www.google.com/maps/place/LRF+Heating+%26+Building+Ltd/@51.7615896,-2.3128954,15z/data=!4m2!3m1!1s0x0:0x60e5a41015b7245?sa=X"
+string = ""
 
 print(re.search("@(.*),(.*),", string).group(2))
